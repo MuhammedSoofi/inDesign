16d74eab47e1168548933dacd7e13959a8e5d128
muhammed soofi <muhammedsoofic@gmail.com>
2024-03-12 10:17:08 +0530

    Added validation to contact form

--- script.js
+++ script.js
@@ -10,3 +10,51 @@ window.addEventListener('scroll',function(){
         section.classList.add('logoSecBg')
      }
 })
+
+
+ 
+
+$(document).ready(
+   
+   $('#contact_form').validate({
+      rules:{
+         name:{
+            required:true
+         },
+         email:{
+            required:true,
+            email:true
+         },
+         message:{
+            required:true,
+            minlength:20
+         }
+      },
+      messages:{
+         message:{
+            required:'let us know about your requirements'
+         }
+      }
+
+   }),
+
+   $('#submitBtn').click(function(){
+   
+      var mobileNumber = $('#mobileNumber').val()
+      var mobileNumberPattern = /^[1-9]\d{9}$/
+
+      if(mobileNumber.match(mobileNumberPattern)){
+         $('#numberError').hide()
+       
+      }
+      else{
+           
+          $('#numberError').text('enter a valid mobile number').css({'color':'red'})
+      }
+      
+   })
+
+   
+
+   
+)
